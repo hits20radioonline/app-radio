083b5f57f971096c52e03b94fd2fc8381fdd7a29
hits20radioonline <hits20radioonline@gmail.com>
2026-08-07 22:21:00 -0300

    Update applicationId in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
  namespace="com.hits20radio.online"; compileSdk=36
- defaultConfig { applicationId="com.hits20radio.online"; minSdk=23; targetSdk=36; versionCode=2; versionName="2.0" }
+ defaultConfig { applicationId="com.hits20radio.online.v2"; minSdk=23; targetSdk=36; versionCode=2; versionName="2.0" }
  buildTypes { release { isMinifyEnabled=false } }
  compileOptions { sourceCompatibility=JavaVersion.VERSION_11; targetCompatibility=JavaVersion.VERSION_11 }
  kotlinOptions { jvmTarget="11" }
