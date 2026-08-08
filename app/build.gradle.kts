020971bc954bc55717da55a23c0b989158dd6a4b
hits20radioonline <hits20radioonline@gmail.com>
2026-08-08 19:16:35 -0300

    Update minSdk version from 23 to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
  namespace="com.hits20radio.online.v2"; compileSdk=36
- defaultConfig { applicationId="com.hits20radio.online.v2"; minSdk=23; targetSdk=36; versionCode=2; versionName="2.0" }
+ defaultConfig { applicationId="com.hits20radio.online.v2"; minSdk=21; targetSdk=36; versionCode=2; versionName="2.0" }
  buildTypes { release { isMinifyEnabled=false } }
  compileOptions { sourceCompatibility=JavaVersion.VERSION_11; targetCompatibility=JavaVersion.VERSION_11 }
  kotlinOptions { jvmTarget="11" }
