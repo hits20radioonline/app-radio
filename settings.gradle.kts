3d23aa35f2d322a4a0a455cc9eb42ad7167a5785
hits20radioonline <hits20radioonline@gmail.com>
2026-08-09 01:26:43 -0300

    Update settings.gradle.kts to include JitPack
    Added JitPack repository to dependency resolution.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,19 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
-dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
-rootProject.name="Hits20RadioOnlineV2"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // <-- REPOSITORIO NECESARIO AGREGADO AQUÍ
+    }
+}
+
+rootProject.name = "Hits20RadioOnlineV2"
 include(":app")
